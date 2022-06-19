90d71da96b1b1891d4f7375f304998c2c7516b29
kchen1999 <kchen174175@gmail.com>
2022-06-19 18:29:28 +1000

    created 16x16 grid of square divs

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 
 const container = document.querySelector('.container');
-const row = document.createElement('div')
-row.classList.add('row-container')
-container.appendChild(row);
 
-for(let j = 0; j < 16; j++) {
-    const square = document.createElement('div'); 
-    square.classList.add('grid-square'); 
+for(let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row-container');
+    container.appendChild(row);
+
+    for(let j = 0; j < 16; j++) {
+        const square = document.createElement('div'); 
+        square.classList.add('grid-square'); 
+        row.appendChild(square); 
+    }
 }
+
+
+
+
 
     
