10589dd446b32311c79cfc225205fd4c85dffa69
kchen1999 <kchen174175@gmail.com>
2022-06-19 17:17:30 +1000

    added flex row container

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+
 const container = document.querySelector('.container');
-for(let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    container.appendChild(row);
+const row = document.createElement('div')
+row.classList.add('row-container')
+container.appendChild(row);
+
+for(let j = 0; j < 16; j++) {
+    const square = document.createElement('div'); 
+    square.classList.add('grid-square'); 
 }
+
+    
